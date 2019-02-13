f405fa787ceea7b985e58fe7ee7c5ac01cddbbfc
David Thayer <dthayer16@gmail.com>
2019-02-13 01:22:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int of % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  elsif int of % 3 && 5 == 0
-    "FizzBuzz"
-  else int == 0
   end
 end
